83848603504912f48f52450f2572b20d75e416c7
charlesx <xiaoyang.charles@gmail.com>
2022-01-18 21:47:58 -0500

    Update by Yang
    Creating a function to check if all students have and only have in one group.

--- code.py
+++ code.py
@@ -1,20 +1,12 @@
-<<<<<<< HEAD
-def are_valid_groups(StudentNumList, GroupList):
-    result = True
-    for i in StudentNumList:
-        for Group in GroupList:
-            if i not in Group:
-                result = False
-    return result
-=======
 def are_valid_groups(studentnumber, groups):
-    List = []
-    for x in groups:
-       for y in x:
-          if y in List:
-            return false
-          else:
-            List.append(y)
-    if studentnumber == List:
-      return true
->>>>>>> 814bc26abe91b50769b995f537bd5f25c7fd204e
+    for group in groups:
+        for members in group:
+            if studentnumber in group:
+                studentnumber.remove(members)
+            else:
+                return False
+    if not studentnumber:
+        return True
+    else:
+        return False
+
